7336ddc4b66a9f95c34b60daed1d7f2476fa9b6a
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-09-02 17:30:22 +0530

    Initial commit

---
+++ YoutubeTags/YoutubeTags.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import html5lib
+import requests 
+
+class Tags:
+      
+      def __init__(url:str):
+      
+         try:
+         	 request = requests.get(url)
+              soup = BeautifulSoup(request.content, 'html5lib') 
+              tags = ', '.join([ meta.attrs.get("content") for meta in soup.find_all("meta",{"property": "og:video:tag"}) ])
+    
+              return tags
+      	
---
+++ YoutubeTags/__init__.py
@@ -0,0 +1,1 @@
+from .YoutubeTags import Tags
