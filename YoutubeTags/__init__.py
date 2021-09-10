eb0c038a47b33aa9ddb1c5916605c48fa6c841e3
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-09-10 06:47:59 +0530

    Update __init__.py

--- YoutubeTags/__init__.py
+++ YoutubeTags/__init__.py
@@ -1,1 +1,14 @@
-from .YoutubeTags import Tags
+from bs4 import BeautifulSoup
+import html5lib
+import requests 
+
+def videotags(url):
+      
+         try:
+              request = requests.get(url)
+              soup = BeautifulSoup(request.content, 'html5lib') 
+              tags = ', '.join([ meta.attrs.get("content") for meta in soup.find_all("meta",{"property": "og:video:tag"}) ])
+              return tags
+      	
+         except:
+              return None
