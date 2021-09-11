8fb7ea91d4ac462e24eaf575941a6e5be3d1da2b
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-09-11 16:13:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (file / "README.md").read_text()
 
 setuptools.setup(
     name="YoutubeTags",
-    version="1.2.2",
+    version="1.2.3",
     author="Nuhman Pk",
     author_email="nuhmanpk7@gmail.com",
     long_description = README,
