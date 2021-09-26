e98611fe585680e21e61cde144607d306be1e1a4
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-26 14:12:23 +0530

    Code format

--- YoutubeTags/__init__.py
+++ YoutubeTags/__init__.py
@@ -3,25 +3,21 @@ import requests
 import bs4
 from bs4 import BeautifulSoup
 
+
 def videotags(url):
-      
-         try:
-              request = requests.get(url)
-              soup = BeautifulSoup(request.content, 'html5lib') 
-              tags = ', '.join([ meta.attrs.get("content") for meta in soup.find_all("meta",{"property": "og:video:tag"}) ])
-              return tags
-      	
-         except:
-              return None
+    try:
+        request = requests.get(url)
+        soup = BeautifulSoup(request.content, 'html5lib') 
+        tags = ', '.join([ meta.attrs.get("content") for meta in soup.find_all("meta",{"property": "og:video:tag"}) ])
+        return tags
+    except:
+        return None
 
 def channeltags(url):
-
-         try:
-              request = requests.get(url)
-              soup = BeautifulSoup(request.content, 'html5lib') 
-              tags = ', '.join([ meta.attrs.get("content") for meta in soup.find_all("meta",{"property": "og:video:tag"}) ])
-              return tags
-      	
-         except:
-              return None
-            
+    try:
+        request = requests.get(url)
+        soup = BeautifulSoup(request.content, 'html5lib') 
+        tags = ', '.join([ meta.attrs.get("content") for meta in soup.find_all("meta",{"property": "og:video:tag"}) ])
+        return tags
+    except:
+        return None
