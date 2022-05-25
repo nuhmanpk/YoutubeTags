fc327b3cbfdca3fefd721069805b1998f3adc1b7
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2022-05-25 16:30:50 +0530

    Code formatted

--- YoutubeTags/__init__.py
+++ YoutubeTags/__init__.py
@@ -1,27 +1,30 @@
 import html5lib
-import requests 
+import requests
 import bs4
 from bs4 import BeautifulSoup
 
+
 def videotags(url):
-      
-         try:
-              request = requests.get(url)
-              soup = BeautifulSoup(request.content, 'html5lib') 
-              tags = ', '.join([ meta.attrs.get("content") for meta in soup.find_all("meta",{"property": "og:video:tag"}) ])
-              return tags
-      	
-         except:
-              return None
+
+    try:
+        request = requests.get(url)
+        soup = BeautifulSoup(request.content, 'html5lib')
+        tags = ', '.join([meta.attrs.get("content") for meta in soup.find_all(
+            "meta", {"property": "og:video:tag"})])
+        return tags
+
+    except:
+        return False
+
 
 def channeltags(url):
 
-         try:
-              request = requests.get(url)
-              soup = BeautifulSoup(request.content, 'html5lib') 
-              tags = ', '.join([ meta.attrs.get("content") for meta in soup.find_all("meta",{"property": "og:video:tag"}) ])
-              return tags
-      	
-         except:
-              return None
-            
+    try:
+        request = requests.get(url)
+        soup = BeautifulSoup(request.content, 'html5lib')
+        tags = ', '.join([meta.attrs.get("content") for meta in soup.find_all(
+            "meta", {"property": "og:video:tag"})])
+        return tags
+
+    except:
+        return False
